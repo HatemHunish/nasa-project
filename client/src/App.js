2815e6dde04bd3f4c318b2fc9530deee4d0492d4
HatemHunish <hatemhunish@gmail.com>
2025-06-24 11:48:51 +0800

    fix plugin error

--- client/src/App.js
+++ client/src/App.js
@@ -8,31 +8,33 @@ import {
 } from "arwes";
 
 import AppLayout from "./pages/AppLayout";
-import { reactPlugin } from './AppInsights';
-import { AppInsightsContext } from '@microsoft/applicationinsights-react-js';
-import { createBrowserHistory } from 'history';
-
+import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
+import { createBrowserHistory } from "history";
+import { appInsights, reactPlugin } from "./AppInsights";
 import { theme, resources, sounds } from "./settings";
 const history = createBrowserHistory();
-reactPlugin._config.extensionConfig[reactPlugin.identifier].history = history;
+appInsights.config.extensionConfig[reactPlugin.identifier].history = history;
+appInsights.loadAppInsights();
 const App = () => {
-  return <AppInsightsContext.Provider value={reactPlugin}>
-    <ThemeProvider theme={createTheme(theme)}>
-      <SoundsProvider sounds={createSounds(sounds)}>
-        <Arwes
-          animate
-          background={resources.background.large}
-          pattern={resources.pattern}>
-          {(anim) => (
-            <Router>
-              <AppLayout show={anim.entered} />
-            </Router>
-          )}
-        </Arwes>
-      </SoundsProvider>
-    </ThemeProvider>
-    ;
-  </AppInsightsContext.Provider>;
+  return (
+    <AppInsightsContext.Provider value={reactPlugin}>
+      <ThemeProvider theme={createTheme(theme)}>
+        <SoundsProvider sounds={createSounds(sounds)}>
+          <Arwes
+            animate
+            background={resources.background.large}
+            pattern={resources.pattern}>
+            {(anim) => (
+              <Router>
+                <AppLayout show={anim.entered} />
+              </Router>
+            )}
+          </Arwes>
+        </SoundsProvider>
+      </ThemeProvider>
+      ;
+    </AppInsightsContext.Provider>
+  );
 };
 
 export default App;
--- client/src/AppInsights.js
+++ client/src/AppInsights.js
@@ -2,16 +2,17 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
 
 const reactPlugin = new ReactPlugin();
+
 const appInsights = new ApplicationInsights({
   config: {
-    instrumentationKey: process.env.REACT_APP_APPINSIGHTS_KEY, // Use .env
+    instrumentationKey: process.env.REACT_APP_APPINSIGHTS_KEY,
     extensions: [reactPlugin],
     extensionConfig: {
-      [reactPlugin.identifier]: { history: null } // Will be set in main App file
+      [reactPlugin.identifier]: {
+        history: null, // set later
+      }
     }
   }
 });
 
-appInsights.loadAppInsights();
-
-export { reactPlugin, appInsights };
+export { appInsights, reactPlugin };
